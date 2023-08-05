436d88dbb13b2c1a6e8ca89b6ea5308135824667
Sushmita Paul <139065061+gitpushSushmita@users.noreply.github.com>
2023-08-05 22:09:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+// script.js
+
+// Show/hide back-to-top button
+window.addEventListener('scroll', function () {
+    const backToTop = document.querySelector('.back-to-top');
+    if (window.scrollY > 300) {
+        backToTop.style.display = 'block';
+    } else {
+        backToTop.style.display = 'none';
+    }
+});
+
+// Smooth scrolling to top
+document.querySelector('.back-to-top a').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
